4a2084f5b0d0e342321ae6b5855a6843c38ed6c7
Hachimnankira <kiran9860819025@gmail.com>
2023-11-12 20:03:33 +0545

    navbar active

--- client/recipe/src/components/navbar.js
+++ client/recipe/src/components/navbar.js
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
 import LogoutIcon from '@mui/icons-material/Logout';
-import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 
 export const Navbar = () => {
     const [cookies, setCookies] = useCookies(["access_token"]);
@@ -16,32 +16,17 @@ export const Navbar = () => {
     }
 
     return (
-        // <div className="navbar">
-        //     <Link to="/">Home</Link>
-        //     <Link to="/create-recipe">Create</Link>
-        //     {!cookies.access_token ?
-        //         (<Link to="/auth">Login</Link>)
-        //         : (
-        //             <>
-        //                 <Link to="/saved-recipe">Saved</Link>
-        //                 <button onClick={logout}> Logout </button>
-        //             </>
-        //         )}
-        // </div>
-
         <div className="navbar-container">
             <div className="navbar">
-                <Link to="/">Home</Link>
-                <Link to="/create-recipe">Create</Link>
+                <NavLink to="/" activeClassName="active">Home</NavLink>
+                <NavLink to="/create-recipe" activeClassName="active">Create</NavLink>
                 {!cookies.access_token ? (
-                    <Link to="/auth">Login</Link>
+                    <NavLink to="/auth" activeClassName="active">Login</NavLink>
                 ) : (
                     <>
-                        <Link to="/saved-recipe">Saved</Link>
-                        {/* <button className="login-btn" onClick={logout}>Logout</button> */}
+                        <NavLink to="/saved-recipe" activeClassName="active">Saved</NavLink>
                         <Button variant="outlined">
-                            <LogoutIcon sx={{ fontSize: 20, color: "#fff"}} onClick={logout}/>
-                            {/* Logout */}
+                            <LogoutIcon sx={{ fontSize: 20, color: "#fff" }} onClick={logout} />
                         </Button>
                     </>
                 )}
